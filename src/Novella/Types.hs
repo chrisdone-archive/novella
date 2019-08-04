39572f750c87cd40483a91b5e546bf1cc237b851
Chris Done <chrisdone@gmail.com>
2019-08-04 16:55:38 +0100

    Small org

--- src/Novella/Types.hs
+++ src/Novella/Types.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveFunctor #-}
 -- | All types for the VTY interface.
 
 module Novella.Types where
 
 import qualified Data.List.NonEmpty as NE
 import Data.List.NonEmpty (NonEmpty(..))
+
+--------------------------------------------------------------------------------
+-- Schemas describing the syntax of a language
+
+-- | Used to name grammar productions, to avoid the confusion of
+-- recursive grammars (no structural equality or show instances).
+newtype SchemaName =
+  SchemaName String
+  deriving (Show, Eq, Ord)
 
 -- | A schema that nodes can implement.
 data Schema
@@ -13,91 +25,16 @@ data Schema
     -- ^ Some literal token that can be lexed by the given lexer.
   | IdentifierSchema !IdentifierCategory
     -- ^ An identifier in the given category.
-  | ChoiceSchema !(NonEmpty Schema)
+  | ChoiceSchema !(NonEmpty SchemaName)
     -- ^ A choice between the given schemas.
-  | ListSchema !Schema !Delimiter
+  | ListSchema !SchemaName !Delimiter
     -- ^ A list of the given schema, separated by delimiter.
-  | CompositeSchema (NonEmpty Schema)
+  | CompositeSchema !(NonEmpty SchemaName)
     -- ^ A composite of the given different schemas.
+  deriving (Show, Eq)
 
--- | State of the app.
-data State
-
--- | Cursor within the tree.
-data Cursor
-
--- | A text query for the right node.
-data Query
-
--- | A slot in the tree.
-data Slot a
-  = EmptySlot
-  | QuerySlot !Query
-  | FilledSlot !a
-
--- | A slot with a type.
-data TypedSlot =
-  TypedSlot
-    { typedSlotSchema :: !Schema
-    , typedSlotSlot :: !(Slot Node)
-    }
-
--- | A node in the tree.
-data Node
-  = KeywordNode !Keyword
-    -- ^ A non-editable keyword.
-  | ListNode !ListEditor
-    -- ^ An arbitrary-sized list of a single schema type of slots.
-  | TupleNode !TupleEditor
-    -- ^ A fixed-size n-tuple of different schema type slots.
-  | CompositeNode !CompositeEditor
-    -- ^ A composite of schemas and a single slot.
-  | IdentifierNode !IdentifierEditor
-    -- ^ A lookup node (variable, constructor, anything named).
-  | TokenNode !TokenEditor
-    -- ^ Token node.
-
--- | Editor for a list.
-data ListEditor =
-  ListEditor
-    { listEditorSchema :: !Schema
-    , listEditorTypedSlots :: ![TypedSlot]
-    , listEditorDelimiter :: !Delimiter
-    }
-
--- | A list delimiter.
-data Delimiter
-
--- | Editor for a tuple.
-data TupleEditor =
-  TupleEditor
-    { tupleEditorTypedSlots :: !(NonEmpty TypedSlot)
-    }
-
--- | Editor for a composite of editors.
-data CompositeEditor =
-  CompositeEditor
-    { compositeEditorSchemas :: !(NonEmpty Schema)
-    , compositeEditorMaybeSlot :: !(Maybe TypedSlot)
-    }
-
--- | Editor for a identifier of editors.
-data IdentifierEditor =
-  IdentifierEditor
-    { identifierEditorIdentifierCategory :: IdentifierCategory
-    , identifierEditorSlot :: !(Slot Identifier)
-    }
-
--- | Editor for an arbitrary string, with a lexer that can be
--- provided.
-data TokenEditor =
-  TokenEditor
-    { tokenEditorLexerName :: !LexerName
-    , tokenEditorMaybeToken :: !(Maybe Token)
-    }
-
--- | Some lexed token.
-newtype Token = Token String
+newtype Delimiter =
+  Delimiter String
   deriving (Show, Eq, Ord)
 
 -- | Lexer name that can be used to find a lexer.
@@ -115,13 +52,122 @@ newtype Keyword =
   Keyword String
   deriving (Show, Eq, Ord)
 
--- | A ident looked up from a scope environment.
-newtype Identifier =
-  Identifier String
-  deriving (Show, Eq, Ord)
+--------------------------------------------------------------------------------
+-- High-level types for the UI
+
+-- | State of the app.
+data State =
+  State
+    { stateTypedSlot :: !TypedSlot
+    , stateCursor :: !Cursor
+    }
+  deriving (Show, Eq)
+
+-- | Cursor pointing to a place within the tree.
+data Cursor
+  = InList !Int !Cursor
+  | InComposite !Int !Cursor
+  | InChoice !Cursor
+  | Here
+  deriving (Show, Eq)
 
 -- | User input; key press, mouse click, etc.
 data Input
 
 -- | Events that cause a change in the state.
 data Event
+
+--------------------------------------------------------------------------------
+-- The UI tree of editors
+
+-- | A slot in the tree.
+data Slot a
+  = EmptySlot
+  | QuerySlot !Query
+  | FilledSlot !a
+  deriving (Show, Eq, Functor, Traversable, Foldable)
+
+-- | A slot with a type.
+data TypedSlot =
+  TypedSlot
+    { typedSlotSchema :: !Schema
+    , typedSlotSlot :: !(Slot Node)
+    }
+  deriving (Show, Eq)
+
+-- | A node in the tree.
+data Node
+  = KeywordNode !Keyword
+    -- ^ A non-editable keyword.
+  | ListNode !ListEditor
+    -- ^ An arbitrary-sized list of a single schema type of slots.
+  | TupleNode !TupleEditor
+    -- ^ A fixed-size n-tuple of different schema type slots.
+  | CompositeNode !CompositeEditor
+    -- ^ A composite of schemas and a single slot.
+  | IdentifierNode !IdentifierEditor
+    -- ^ A lookup node (variable, constructor, anything named).
+  | TokenNode !TokenEditor
+    -- ^ Token node.
+    deriving (Show, Eq)
+
+--------------------------------------------------------------------------------
+-- Editors
+
+-- | Editor for a list.
+data ListEditor =
+  ListEditor
+    { listEditorSchema :: !Schema
+    , listEditorTypedSlots :: ![TypedSlot]
+    , listEditorDelimiter :: !Delimiter
+    }
+  deriving (Show, Eq)
+
+-- | Editor for a tuple.
+data TupleEditor =
+  TupleEditor
+    { tupleEditorTypedSlots :: !(NonEmpty TypedSlot)
+    }
+  deriving (Show, Eq)
+
+-- | Editor for a composite of editors.
+data CompositeEditor =
+  CompositeEditor
+    { compositeEditorSchemas :: !(NonEmpty Schema)
+    , compositeEditorMaybeSlot :: !(Maybe TypedSlot)
+    }
+  deriving (Show, Eq)
+
+-- | Editor for a identifier of editors.
+data IdentifierEditor =
+  IdentifierEditor
+    { identifierEditorIdentifierCategory :: IdentifierCategory
+    , identifierEditorSlot :: !(Slot Identifier)
+    }
+  deriving (Show, Eq)
+
+-- | Editor for an arbitrary string, with a lexer that can be
+-- provided.
+data TokenEditor =
+  TokenEditor
+    { tokenEditorLexerName :: !LexerName
+    , tokenEditorMaybeToken :: !(Maybe Token)
+    }
+  deriving (Show, Eq)
+
+--------------------------------------------------------------------------------
+-- UI auxilliary types
+
+-- | Some lexed token.
+newtype Token = Token String
+  deriving (Show, Eq, Ord)
+
+-- | A ident looked up from a scope environment.
+newtype Identifier =
+  Identifier String
+  deriving (Show, Eq, Ord)
+
+-- | A text query for the right node.
+newtype Query =
+  Query String
+  deriving (Show, Eq, Ord)
