b6f3630eb14de460d6f29f4d5ad4d7fe1b006204
Chris Done <chrisdone@gmail.com>
2019-08-06 14:19:36 +0100

    Depend on streaming-parsers

--- src/Novella.hs
+++ src/Novella.hs
@@ -1,3 +1,16 @@
-module Novella
-    (
-    ) where
+module Novella where
+
+import           Control.Monad.Trans.State.Strict (StateT)
+import qualified Data.Reparsec as Reparsec
+import qualified Data.Reparsec.Sequence as Reparsec
+import           Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import           Novella.Types
+
+-- | Produce an event from an input. If no [valid] event is detected
+eventParser :: Input -> Reparsec.ParserT (Seq Input) e m Event
+eventParser = undefined
+
+-- | Transform the state given the config and the event.
+transformState :: Monad m => Config -> Event -> StateT State m ()
+transformState config event = pure ()
