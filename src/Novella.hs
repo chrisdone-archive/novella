8b070872f762335c991049da39269ae78bcdb46d
Chris Done <chrisdone@gmail.com>
2019-08-19 13:27:52 +0100

    More beauty

--- src/Novella.hs
+++ src/Novella.hs
@@ -6,36 +6,19 @@ module Novella
   , transformState
   ) where
 
-import           Control.Monad.Trans
 import           Control.Monad.Trans.Reader (ReaderT)
 import           Control.Monad.Trans.State.Strict (StateT)
 import           Data.Conduit
 import           Data.Reparsec (ParserT)
-import qualified Data.Reparsec as Reparsec
+import qualified Data.Reparsec.Sequence as Reparsec
 import           Data.Sequence (Seq)
-import qualified Data.Sequence as Seq
 import           Novella.Types
 
 -- | Produce a stream of commands from a stream of inputs.
 commandConduit ::
      Monad m
   => ConduitT Input (Either CommandParseError Command) (ReaderT State m) ()
-commandConduit = go (Reparsec.parseResultT commandParser)
-  where
-    go parser = do
-      v <- await
-      result <- lift (parser (fmap pure v))
-      case result of
-        Reparsec.Done input pos _more command -> do
-          mapM_ leftover (Seq.drop pos input)
-          yield (Right command)
-          commandConduit
-        Reparsec.Failed remaining pos _more errors -> do
-          mapM_ leftover (Seq.drop pos remaining)
-          yield (Left errors)
-          commandConduit
-        Reparsec.Partial cont ->
-          go cont
+commandConduit = Reparsec.parseConduit commandParser
 
 -- | Produce a command from a stream of inputs.
 commandParser ::
